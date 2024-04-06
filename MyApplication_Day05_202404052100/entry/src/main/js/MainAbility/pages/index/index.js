fb81b571549e08423b75b996dc322791a2f3c9ae
DeChao Zhu <15939959176@163.com>
2024-04-06 13:58:39 +0800

    12

--- MyApplication_Day05_202404052100/entry/src/main/js/MainAbility/pages/index/index.js
+++ MyApplication_Day05_202404052100/entry/src/main/js/MainAbility/pages/index/index.js
@@ -3,8 +3,53 @@ export default {
         title: ""
     },
     onInit() {
-        this.title = this.$t('strings.world');
+
+
+        function aa() {
+            return new Promise((resolvo, reject) => {
+                let random = Math.floor(Math.random() * 10) + 1;
+                // @ts-ignore
+                if (random % 2!=0) {
+                    resolvo(random)
+                }
+                else {
+                    reject(`fail number is ${random} `)
+                }
+
+            })
+
+        }
+
+        let myp =aa();
+        myp.then(res=>{
+
+            console.log("succes num is ",res)
+        })
+            .catch(res=>{
+
+                console.log("fail num is ",res)
+            })
+
+       async  function add(x,y){
+           await console.log('add', x + y)
+        }
+
+        add(2,3)
+
+
+
+
+        async  function add2(){
+           return '1'
+        }
+
+        add2().then(tes=>{
+            console.log('add2',tes)
+        })
+
     }
+
+
 }
 
 
